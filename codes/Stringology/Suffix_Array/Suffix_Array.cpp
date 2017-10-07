c58e71df52bc22606b78704d76a7441b9f639fe5
domen111 <domen.public@gmail.com>
2017-10-07 16:51:41 +0800

    change suffix array code

--- codes/Stringology/Suffix_Array/Suffix_Array.cpp
+++ codes/Stringology/Suffix_Array/Suffix_Array.cpp
@@ -1,50 +1,33 @@
-const int MAX = 1020304;
-int ct[MAX], he[MAX], rk[MAX], sa[MAX], tsa[MAX], tp[MAX][2];
-
-void suffix_array(char *ip){
-
-	int len = strlen(ip);
-	int alp = 256;
-
-	memset(ct, 0, sizeof(ct));
-	for(int i=0;i<len;i++) ct[ip[i]+1]++;
-	for(int i=1;i<alp;i++) ct[i]+=ct[i-1];
-	for(int i=0;i<len;i++) rk[i]=ct[ip[i]];
-
-	for(int i=1;i<len;i*=2){
-		for(int j=0;j<len;j++){
-			if(j+i>=len) tp[j][1]=0;
-			else tp[j][1]=rk[j+i]+1;
-			
-			tp[j][0]=rk[j];
-		}
-		memset(ct, 0, sizeof(ct));
-		for(int j=0;j<len;j++) ct[tp[j][1]+1]++;
-		for(int j=1;j<len+2;j++) ct[j]+=ct[j-1];
-		for(int j=0;j<len;j++) tsa[ct[tp[j][1]]++]=j;
-
-		memset(ct, 0, sizeof(ct));
-		for(int j=0;j<len;j++) ct[tp[j][0]+1]++;
-		for(int j=1;j<len+1;j++) ct[j]+=ct[j-1];
-		for(int j=0;j<len;j++) sa[ct[tp[tsa[j]][0]]++]=tsa[j];
-
-		rk[sa[0]]=0;
-		for(int j=1;j<len;j++){
-			if( tp[sa[j]][0] == tp[sa[j-1]][0] &&
-				tp[sa[j]][1] == tp[sa[j-1]][1] )
-				rk[sa[j]] = rk[sa[j-1]];
-			else
-				rk[sa[j]] = j;
-		}
+const int N = 1.12e5, M = 11;
+const char nil = 'a' - 1;
+char s[N];
+int sa[N], ct[N], w[2][N], rk[N], ht[N];
+void suffix_array(int n, int m)
+{
+	int i, j, p = 0, * x = w[0], * y = w[1], h;
+	memset(ct, 0, m * sizeof(int));
+	for (i = 0; i < n; ++i) ++ct[x[i] = s[i] - ('a' - 1)];
+	for (i = 1; i < m; ++i) ct[i] += ct[i - 1];
+	for (i = n - 1; i >= 0; --i) sa[--ct[x[i]]] = i;
+	for (j = p = 1; p < n; j *= 2, m = p) {
+		for (i = n - j, p = 0; i < n; ++i) y[p++] = i;
+		for (i = 0; i < n; ++i) if (sa[i] >= j) y[p++] = sa[i] - j;
+		memset(ct, 0, m * sizeof(int));
+		for (i = 0; i < n; ++i) ++ct[x[y[i]]];
+		for (i = 1; i < m; ++i) ct[i] += ct[i - 1];
+		for (i = n - 1; i >= 0; --i) sa[--ct[x[y[i]]]] = y[i];
+		swap(x, y);
+		x[sa[0]] = 0;
+		p = 1;
+		for (i = 1; i < n; ++i)
+			x[sa[i]] = y[sa[i]] == y[sa[i-1]] && y[sa[i] + j] == y[sa[i-1] + j] ? p - 1 : p++;
 	}
-
-	for(int i=0,h=0;i<len;i++){
-		if(rk[i]==0) h=0;
-		else{
-			int j=sa[rk[i]-1];
-			h=max(0,h-1);
-			for(;ip[i+h]==ip[j+h];h++);
-		}
-		he[rk[i]]=h;
+	for (i = 0; i < n; ++i) rk[sa[i]] = i;
+	ht[0] = 0;
+	h = 0;
+	for (i = 0; rk[i]; ++i) {
+		while (s[i + h] == s[sa[rk[i] - 1] + h]) ++h;
+		ht[rk[i]] = h;
+		if (h) --h;
 	}
 }
