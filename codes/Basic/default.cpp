0d6a9c3c53d046d730ae1b33b69351178c83fb12
domen111 <domen111@gmail.com>
2018-09-24 17:02:27 +0800

    Update default code add BG (debug) micro

--- codes/Basic/default.cpp
+++ codes/Basic/default.cpp
@@ -1,39 +1,43 @@
 #pragma GCC optimize ("O2")
+#pragma GCC target("avx,tune=native")
 #include<bits/stdc++.h>
-#include<unistd.h>
+
+#define ALL(a) begin(a), end(a)
+
 using namespace std;
-#define FZ(n) memset((n),0,sizeof(n))
-#define FMO(n) memset((n),-1,sizeof(n))
-#define F first
-#define S second
-#define PB push_back
-#define ALL(x) begin(x),end(x)
-#define SZ(x) ((int)(x).size())
-#define IOS ios_base::sync_with_stdio(0); cin.tie(0)
-#define REP(i,x) for (int i=0; i<(x); i++)
-#define REP1(i,a,b) for (int i=(a); i<=(b); i++)
-#ifdef ONLINE_JUDGE
-#define FILEIO(name) \
-    freopen(name".in", "r", stdin); \
-    freopen(name".out", "w", stdout);
+
+// deBuG function
+// example:
+// int a = 3; int b[] = {1,2}; string c = "abc";
+// BG(a, b[1], c); // stderr: int main():37: a = 3,  b[1] = 2,  c = a
+#ifdef LITE
+
+void _BG(const char * s) {cerr<<s<<endl;}
+template<class T, class ... TT>
+void _BG(const char * s,T a, TT...b)
+{
+    for (size_t c = 0; *s && (c || *s != ','); cerr<<*s++)
+        c += count(ALL("([{"), *s) - count(ALL(")]}"), *s); // Implementation defined. But that's ok since it only runs on local.
+    cerr<<" = "<<a;
+    if (*s) {
+        cerr<<", ";
+        ++s;
+    }
+    _BG(s,b...);
+}
+#define BG(...) do { \
+    cerr << __PRETTY_FUNCTION__ << ':' << __LINE__ << ": "; \
+    _BG(#__VA_ARGS__,__VA_ARGS__); \
+} while(0)
+
 #else
-#define FILEIO(name)
+#define BG(...)
 #endif
-template<typename A, typename B>
-ostream& operator <<(ostream &s, const pair<A,B> &p) {
-    return s<<"("<<p.first<<","<<p.second<<")";
-}
-template<typename T>
-ostream& operator <<(ostream &s, const vector<T> &c) {
-    s<<"[ ";
-    for (auto it : c) s << it << " ";
-    s<<"]";
-    return s;
-}
-// Let's Fight!
 
-int main() {
-    IOS;
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
 
-    return 0;
+    // lite !
 }
